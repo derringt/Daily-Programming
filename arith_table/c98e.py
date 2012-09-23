0416e44d19b9189c1aaeaf6dfb1f7dc14d8f298a
Drew Errington <derringt@gmail.com>
2012-09-23 16:11:43 -0400

    Added div by 0 handling

--- arith_table/c98e.py
+++ arith_table/c98e.py
@@ -7,14 +7,21 @@ args=parser.parse_args()
 
 n = args.integer
 op = args.operand
+start_int = 0
 
+if op == '/':
+	start_int = 1
+	
 table = [[0]*(n+1) for i in range(n+1)]
 div = ''
 run = [i for i in range(n+1)]
 for i in range (n+1):
-	div += '----'
+	div += '---'
 	for j in range(n+1):
-		table[i][j] = eval('%d%s%d'%(i, op, j))
+		if j < start_int:
+			table[i][j] = 'n/a'
+		else:
+			table[i][j] = eval('%d%s%d'%(i, op, j))
 print op, '|', str(run).strip('[]').replace(',',' ')
 print div
 for i in run:

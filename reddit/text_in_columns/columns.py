0f4bd7bae53a36137422aa8537d830a0177a73e1
Drew Errington <drew@errington.xyz>
2014-10-01 13:29:06 -0400

    Added comments

--- reddit/text_in_columns/columns.py
+++ reddit/text_in_columns/columns.py
@@ -1,22 +1,34 @@
+#Reddit Challenge 182, http://redd.it/2hssx6
+#Takes text from input.txt, formats it into columns
 f = open('input.txt')
 
+#First line in format:
+#<number of colums> <column maximum width> <spacees between maxwidth columns>
 cols, colwidth, spacewidth = [int(x) for x in f.readline().rstrip().split(' ')]
+
 build = ['']
 
+#Start building column-width text
 for line in f:
     words = line.split(' ')
     for w in words:
+        #New row if the word is too long
         if len(build[-1]) + len(w.rstrip()) + 1 > colwidth:
                 build.append('')
+        #Add spaces between words
         if len(build[-1]) > 0:
             build[-1] += ' '
         build[-1] += w.rstrip()
+        #When you hit a newline, make a new row
         if '\n' in w:
             build.append('')
 
+#Calculate number of rows, making it as even as possible.
 rows = (len(build) / cols) + (len(build) % cols)
+
 final = []
 
+#Pad out the columns so we don't overflow
 for x in xrange(0,cols+1):
     build.append('')
 
@@ -24,6 +36,7 @@ for r in xrange(0,rows):
     final.append('')
     for c in xrange(0, cols):
         final[-1] += build[r + c*rows] 
+        #Add spaces between columns
         final[-1] += ' ' * (colwidth - len(build[r+c*rows]) + spacewidth)
 
 o = open('output.txt', 'w')
